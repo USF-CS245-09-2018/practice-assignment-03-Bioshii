68f12a4212fe9b88ab0c71c7e97a36696de61399
Bio <bioshimv@gmail.com>
2018-09-14 09:53:39 -0700

    Ready for grading

--- Practice03Test.java
+++ Practice03Test.java
@@ -54,7 +54,9 @@ public class Practice03Test {
 
 	public int find_min_iterative () {
 		// TODO: Fill in this iterative function.
+		// Set the minimum value index to 0, to start the function
 		int minIndex = 0; // O(1) - Constant - Only happens once
+		// Iterate through the array, comparing each value to current min value. Replace if current index value is less than minIndex value
 		for (int currentIndex = 1; currentIndex < arr.length; currentIndex++) { // O(1) since we only setup once
 			if (arr[currentIndex] < arr[minIndex]) { //O(n) since we perform this comparison the amount of times there are items, aka 'n'
 				minIndex = currentIndex; //O(n) Worst case we have to replace min value index every time
@@ -67,18 +69,26 @@ public class Practice03Test {
 
 	public int find_min_recursive () {
 		// TODO: Fill in this recursive function.
+		// minIndex at 0 to simply start comparisons
 		int minIndex = 0; // O(1) - Constant - Only happens once
+		// pleaseEnd will be the current value we're comparing as well as the comparison for base case
 		int pleaseEnd = 1; // O(1) - Constant - Only happens once
+
+		// Call the overloaded recursive function with necessary paramaters
 		return find_min_recursive(minIndex, arr, pleaseEnd); // O(1) - Constant - Only happens once
 	}
 
+
 	public int find_min_recursive(int minIndex, double[] arr, int pleaseEnd) { //O(1) * n(after first call) = O(n)
+		// Each time we recurse, pleaseEnd increments. Once it equals the array length, that means it has gone through the entire array. So return minIndex.
 		if (pleaseEnd == arr.length) { // O(n) Comparison happens n times
 			return minIndex; // O(1) - Constant - Once called, will never happen again
 		} else {
+			// If current index or pleaseEnd is less than current minIndex, replace the minIndex
 			if (arr[pleaseEnd] < arr[minIndex]) { // O(n) - Worst cose scenario, happens for every item
 				minIndex = pleaseEnd; // O(n) - Worst case scenario, happens every time
 			}
+			// Recurse until the entire array has been gone through
 			return find_min_recursive(minIndex, arr, pleaseEnd+1); // O(n) - Worst case scenario, happens every time
 		}
 	}
@@ -86,7 +96,7 @@ public class Practice03Test {
 	/*
 		Iterative Total: O(1 + 1 + n + n) = O(2n + 2)
 		Recursive Total: O(1 + 1 + 1 + n + 1 + n + n + n) = O(4 + 4n)
-	
+
 	*/
 
 	/**
